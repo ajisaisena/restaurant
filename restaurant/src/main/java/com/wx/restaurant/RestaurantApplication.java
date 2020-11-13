06c920d7dbe2f80b8a79ce60b00b91df70f0ffbd
fremakvi <473768310@qq.com>
2020-11-13 10:57:24 +0800

    new the git for windows

--- restaurant/src/main/java/com/wx/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/com/wx/restaurant/RestaurantApplication.java
@@ -17,8 +17,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class RestaurantApplication {
     //TODO: 改微信后台的服务器，以使真机连接到服务器上
     //TODO: 询问是否需要真机使用。
-    //TODO: 增加数据库页面和菜单图片
-    //TODO: 增加数据库优惠券数据
     public static void main(String[] args) {
         SpringApplication.run(RestaurantApplication.class, args);
     }
